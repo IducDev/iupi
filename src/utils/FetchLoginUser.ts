bb33e366d02e7aea600793fb2918d95688b4972f
iducDev <urdanetacuarteisaacdavid@gmail.com>
2025-01-20 10:53:08 -0400

    first commit

---
+++ src/utils/FetchLoginUser.ts
@@ -0,0 +1,29 @@
+
+export  interface LoginUser {   
+    email: string;
+    password: string; 
+}
+
+
+export const fetchLoginUser = async (userData: LoginUser) => {
+
+    try {
+        const response = await fetch(`https://fintech-ggjf.onrender.com/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
