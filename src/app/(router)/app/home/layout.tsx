0ba43d8beb8315729b1c69aa77528b641dc563eb
iducDev <urdanetacuarteisaacdavid@gmail.com>
2025-01-23 10:26:47 -0400

    working yet

--- src/app/(router)/app/home/layout.tsx
+++ src/app/(router)/app/home/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@/components/layout/Navbar';
+// import Navbar from '@/components/layout/Navbar';
 import React from 'react';
 
 
@@ -7,7 +7,6 @@ import React from 'react';
 const HomeLayout = ({ children }:{children: React.ReactNode}) => {
     return (
         <div>
-            <Navbar/>
             {children}
         </div>
     );
